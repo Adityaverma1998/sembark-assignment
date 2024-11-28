8c6499b44268f729bc0f74c3bd164e6c39a4b175
aditya <aditya@bucle.dev>
2024-11-29 01:53:37 +0530

    mod: add toast

--- src/components/cart-items/index.tsx
+++ src/components/cart-items/index.tsx
@@ -3,22 +3,27 @@ import AddIcon from "@mui/icons-material/Add";
 import {CartItem} from "../../interface";
 import {useContext} from "react";
 import {CartContext} from "../../context";
+import {toast} from "react-toastify";
 
 const CartItems = (props:CartItem)=>{
     const {image,name,price,id} = props;
     const cartContext = useContext(CartContext);
     const {cartItemsQuantityIncrement,cartItemsQuantityDecrement,removeFromCart} = cartContext;
 
-    const cartIncrement = ()=>{
+    const cartIncrement = () => {
         cartItemsQuantityIncrement(id);
-    }
-    const cartDecrement = ()=>{
-        cartItemsQuantityDecrement(id);
+        toast.success(`${name} quantity increased!`);
+    };
 
-    }
-    const removeItem =()=>{
+    const cartDecrement = () => {
+        cartItemsQuantityDecrement(id);
+        toast.success(`${name} quantity decreased!`);
+    };
+
+    const removeItem = () => {
         removeFromCart(id);
-    }
+        toast.error(`${name} has been removed from your cart!`);
+    };
 
     return(
         <>
@@ -30,15 +35,15 @@ const CartItems = (props:CartItem)=>{
                     </div>
                     <div>
                         <h3 className="text-base font-bold text-gray-800">{name}</h3>
-                        <h6 className="text-xs text-red-500 cursor-pointer mt-0.5">Remove</h6>
+                        <h6 className="text-xs text-red-500 cursor-pointer mt-0.5" onClick={removeItem}>Remove</h6>
                         <div className="flex gap-4 mt-4">
 
                             <div>
                                 <button type="button"
                                         className="flex items-center px-2.5 py-1.5 border border-gray-300 text-gray-800 text-xs outline-none bg-transparent rounded-md">
-                                    <RemoveIcon className="w-2.5 fill-current"/>
+                                    <RemoveIcon className="w-2.5 fill-current" onClick={cartDecrement}/>
                                     <span className="mx-2.5">1</span>
-                                    <AddIcon className="w-2.5 fill-current"/>
+                                    <AddIcon className="w-2.5 fill-current" onClick={cartIncrement}/>
                                 </button>
                             </div>
                         </div>
