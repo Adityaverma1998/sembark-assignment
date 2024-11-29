16b5f0656b3b9d3ea9a3665a434b9191eef9e860
aditya <aditya@bucle.dev>
2024-11-29 12:38:52 +0530

    fix: product details page loading

--- src/components/product-loader/product-detail-page-loader/index.tsx
+++ src/components/product-loader/product-detail-page-loader/index.tsx
@@ -1,28 +1,24 @@
 const ProductDetailsPageShimmer = () => {
     return (
-        <div className="max-w-xs mx-auto overflow-hidden rounded-lg shadow-md bg-white w-[300px] aspect-w-9 aspect-h-16">
-            <div className="flex flex-wrap">
-                {/* Image Skeleton */}
-                <div className="relative w-full h-48 sm:h-64 md:h-72 lg:h-80 bg-gray-300 animate-pulse">
-                    <div className="absolute inset-0 bg-gradient-to-r from-gray-300 via-gray-200 to-gray-300 animate-pulse"></div>
+        <div className="overflow-hidden rounded-lg shadow-md bg-white flex space-x-4 p-4 animate-pulse gap-4">
+            {/* Image Skeleton */}
+            <div className="w-[495px] h-[629px] bg-gray-300 relative">
+                <div className="absolute inset-0 bg-gradient-to-r from-gray-300 via-gray-200 to-gray-300"></div>
+            </div>
+
+            <div className="w-1/3 h-auto h-full   self-center space-y-6">
+                {/* Title Skeleton */}
+                <div className="h-8 bg-gray-300 rounded w-3/4"></div>
+
+                <div className="h-6 bg-gray-300 rounded w-1/3"></div>
+
+                <div className="space-y-4">
+                    <div className="h-4 bg-gray-300 rounded w-2/3"></div>
+                    <div className="h-4 bg-gray-300 rounded w-3/4"></div>
+                    <div className="h-4 bg-gray-300 rounded w-1/2"></div>
                 </div>
 
-                {/* Details Skeleton */}
-                <div className="p-4 space-y-4">
-                    {/* Title Skeleton */}
-                    <div className="h-6 bg-gray-300 rounded w-3/4 animate-pulse"></div>
-
-                    {/* Price Skeleton */}
-                    <div className="h-4 bg-gray-300 rounded w-1/3 animate-pulse"></div>
-
-                    {/* Description Skeleton */}
-                    <div className="h-4 bg-gray-300 rounded w-2/3 animate-pulse"></div>
-                    <div className="h-4 bg-gray-300 rounded w-3/4 animate-pulse"></div>
-                    <div className="h-4 bg-gray-300 rounded w-1/2 animate-pulse"></div>
-
-                    {/* Add to Cart Button Skeleton */}
-                    <div className="h-10 bg-gray-300 rounded w-full animate-pulse"></div>
-                </div>
+                <div className="h-12 bg-gray-300 rounded w-full"></div>
             </div>
         </div>
     );
